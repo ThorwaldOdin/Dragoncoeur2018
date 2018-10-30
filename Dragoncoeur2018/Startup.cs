0c07dcecfc8f8b5611a8c9cbd029e03d0e647231
ThorwaldOdin <ThorwaldOdin@ODIN>
2018-10-30 22:57:12 +0100

    Aggiungere i file di progetto.

---
+++ Dragoncoeur2018/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dragoncoeur2018.Startup))]
+namespace Dragoncoeur2018
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
